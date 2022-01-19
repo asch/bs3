8c052f81f0bd60b0f70388d94ca1d0b6d0515596
Vojtech Aschenbrenner <v@asch.cz>
2022-01-19 22:51:23 +0100

    Finish nbd backend implementation

--- internal/nbd/nbd.go
+++ internal/nbd/nbd.go
@@ -1,29 +1,59 @@
 package nbd
 
 import (
-	"fmt"
+	"encoding/binary"
 
+	"github.com/asch/bs3/internal/bs3/mapproxy"
+	"github.com/asch/bs3/internal/config"
 	"libguestfs.org/libnbd"
 )
 
+const (
+	WRITE_ITEM_SIZE = 32
+)
+
 type nbd struct {
-	handle *libnbd.Libnbd
+	handle          *libnbd.Libnbd
+	metadata_size   int
+	write_item_size int
 }
 
 func NewNbd() *nbd {
-	return &nbd{}
+	return &nbd{
+		metadata_size:   config.Cfg.Write.ChunkSize / config.Cfg.BlockSize * WRITE_ITEM_SIZE,
+		write_item_size: WRITE_ITEM_SIZE,
+	}
+}
+
+// Parses write extent information from 32 bytes of raw memory. The memory is
+// one write in metadata section of the object.
+func parseExtent(b []byte) mapproxy.Extent {
+	return mapproxy.Extent{
+		Sector: int64(binary.LittleEndian.Uint64(b[:8]) * 512 / uint64(config.Cfg.BlockSize)),
+		Length: int64(binary.LittleEndian.Uint64(b[8:16]) * 512 / uint64(config.Cfg.BlockSize)),
+		SeqNo:  int64(binary.LittleEndian.Uint64(b[16:24])),
+		Flag:   int64(binary.LittleEndian.Uint64(b[24:32])),
+	}
 }
 
 func (n *nbd) BuseWrite(writes int64, chunk []byte) error {
-	// TODO
+	metadata := chunk[:n.metadata_size]
+
+	data := chunk[n.metadata_size:]
+	for i := int64(0); i < writes; i++ {
+		e := parseExtent(metadata[:n.write_item_size])
+
+		n.handle.Pwrite(data[:e.Length*512], uint64(e.Sector*512), nil)
+
+		metadata = metadata[n.write_item_size:]
+		data = data[e.Length*512:]
+	}
+
 	return nil
 }
 
 func (n *nbd) BuseRead(sector, length int64, chunk []byte) error {
-	fmt.Println("Reading" + string(sector))
 	err := n.handle.Pread(chunk, uint64(sector), nil)
-	fmt.Println(err)
-
 	return err
 }
 
