9d5e9db41de1174679df00a539fbda6d824fc10a
Vojtech Aschenbrenner <v@asch.cz>
2022-01-24 11:07:43 +0100

    Add nbd multiconn round-robin implementation

--- internal/nbd/nbd.go
+++ internal/nbd/nbd.go
@@ -2,6 +2,7 @@ package nbd
 
 import (
 	"encoding/binary"
+	"sync"
 
 	"github.com/asch/bs3/internal/bs3/mapproxy"
 	"github.com/asch/bs3/internal/config"
@@ -13,16 +14,22 @@ const (
 )
 
 type nbd struct {
-	handle          *libnbd.Libnbd
+	i               int64
+	m               sync.Mutex
+	handles         []*libnbd.Libnbd
 	metadata_size   int
 	write_item_size int
 }
 
 func NewNbd() *nbd {
-	return &nbd{
+	n := nbd{
 		metadata_size:   config.Cfg.Write.ChunkSize / config.Cfg.BlockSize * WRITE_ITEM_SIZE,
 		write_item_size: WRITE_ITEM_SIZE,
 	}
+
+	n.handles = make([]*libnbd.Libnbd, config.Cfg.Threads)
+
+	return &n
 }
 
 // Parses write extent information from 32 bytes of raw memory. The memory is
@@ -37,13 +44,21 @@ func parseExtent(b []byte) mapproxy.Extent {
 }
 
 func (n *nbd) BuseWrite(writes int64, chunk []byte) error {
+	n.m.Lock()
+	localI := n.i
+	n.i++
+	n.m.Unlock()
+
+	localI %= int64(config.Cfg.Threads)
+
 	metadata := chunk[:n.metadata_size]
 
 	data := chunk[n.metadata_size:]
+	h := n.handles[localI]
 	for i := int64(0); i < writes; i++ {
 		e := parseExtent(metadata[:n.write_item_size])
 
-		n.handle.Pwrite(data[:e.Length*512], uint64(e.Sector*512), nil)
+		h.Pwrite(data[:e.Length*512], uint64(e.Sector*512), nil)
 
 		metadata = metadata[n.write_item_size:]
 		data = data[e.Length*512:]
@@ -53,23 +68,30 @@ func (n *nbd) BuseWrite(writes int64, chunk []byte) error {
 }
 
 func (n *nbd) BuseRead(sector, length int64, chunk []byte) error {
-	err := n.handle.Pread(chunk, uint64(sector), nil)
-	return err
+	//err := n.handles[n.i].Pread(chunk, uint64(sector), nil)
+	//n.i = (n.i + 1) % config.Cfg.Threads
+	//return err
+	return nil
 }
 
 func (n *nbd) BusePreRun() {
 	var err error
-	n.handle, err = libnbd.Create()
-	if err != nil {
-		panic(err)
-	}
+	for i := 0; i < config.Cfg.Threads; i++ {
+		n.handles[i], err = libnbd.Create()
+		if err != nil {
+			panic(err)
+		}
 
-	err = n.handle.ConnectUnix("/tmp/nbd.sock")
-	if err != nil {
-		panic(err)
+		h := n.handles[i]
+		err = h.ConnectUnix("/tmp/nbd.sock")
+		if err != nil {
+			panic(err)
+		}
 	}
 }
 
 func (n *nbd) BusePostRemove() {
-	n.handle.Close()
+	for i := 0; i < config.Cfg.Threads; i++ {
+		n.handles[i].Close()
+	}
 }
